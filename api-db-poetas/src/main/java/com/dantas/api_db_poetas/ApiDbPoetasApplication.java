f5a514ea9795a32de60a0c6940b24d5e7aeb113d
carlosdantas <cjdantasoliveira@gmail.com>
2024-10-07 21:48:32 -0300

    Subindo a aplicacao

---
+++ api-db-poetas/src/main/java/com/dantas/api_db_poetas/ApiDbPoetasApplication.java
@@ -0,0 +1,13 @@
+package com.dantas.api_db_poetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDbPoetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDbPoetasApplication.class, args);
+	}
+
+}
---
+++ api-db-poetas/src/test/java/com/dantas/api_db_poetas/ApiDbPoetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantas.api_db_poetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDbPoetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
